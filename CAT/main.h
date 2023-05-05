a0de19c74ef052f6b9044c46b55ac54fcf5625cc
thaksin5 <114573651+thaksin5@users.noreply.github.com>
2023-05-06 03:12:02 +0800

    add game

---
+++ CAT/main.h
@@ -0,0 +1,227 @@
+﻿#include "DxLib.h"
+#include <stdio.h>
+#include <math.h>
+#include <string>
+using namespace std;
+
+void loadg();
+
+//String 使用 (Using Strings)
+
+//プログラム中 (in program)
+//main-10
+//タイトル-100 (Title-100)
+int main=100,maintm=0;
+
+//ステージ (stage)
+int stagecolor=0;
+int sta=1,stb=4,stc=0;
+
+//クイック (fast or speed) 
+int fast=1;
+
+//トラップ表示 (trap display)
+int trap=1;
+
+//中間ゲート (middle gate)
+int tyuukan=0;
+
+
+//スタッフロール (stuffed roll)
+int ending=0;
+
+
+//ステージ読み込みループ(いじらない) (Stage loading loop (no fiddling))
+int stagerr,stagepoint;
+//オーバーフローさせる (overflow)
+int over=0;
+
+//ステージスイッチ stage switch
+int stageonoff=0;
+
+
+//メインプログラム main program
+void Mainprogram();
+void rpaint();
+int maint;
+
+
+//サブクラス subclass
+//(ウエイト系 (我不知道英文怎麽翻譯) 
+void wait(int interval);
+void wait2(long stime, long etime,int FLAME_TIME);
+int rand(int Rand);
+void end();
+
+//描画 drawing
+int color;
+void setfont(int a);
+void setcolor(int red, int green, int blue);
+void setc0();
+void setc1();
+void drawpixel(int a,int b);
+void drawline(int a,int b,int c,int d);
+void drawrect(int a,int b,int c,int d);
+void fillrect(int a,int b,int c,int d);
+void drawarc(int a,int b,int c,int d);
+void fillarc(int a,int b,int c,int d);
+int grap[161][8],mgrap[51];
+int loadimage(string b);
+int loadimage(int a,int x,int y,int r,int z);
+int mirror;
+void drawimage(int mx,int a,int b);
+void drawimage(int mx,int a,int b,int c,int d,int e,int f);
+void setre();
+void setre2();
+void setno();
+int oto[151];
+void ot(int x);void bgmchange(int x);
+
+//文字 character
+void str(string c,int a,int b);
+
+
+//)
+
+void stagecls();
+void stage();
+void stagep();
+
+
+
+
+
+//1-ステージ （1-Stage）
+//10-ステージ前 (10-before stage)
+//
+
+
+
+//ループ (loop)
+int t,tt,t1,t2,t3,t4;
+
+
+//初期化 Initialization
+int zxon,zzxon;
+
+//キーコンフィグ (key configuration or key setting)
+int key,keytm;
+
+//三角関数 Trigonometric function
+double pai=3.1415926535;
+
+
+//地面 Ground
+#define smax 31
+int sx,sco;
+int sa[smax],sb[smax],sc[smax],sd[smax],stype[smax],sxtype[smax],sr[smax];
+int sgtype[smax];
+
+
+
+//プレイヤー player
+int mainmsgtype;
+int ma,mb,mnobia,mnobib,mhp;
+int mc,md,macttype,atkon,atktm,mactsok,msstar,nokori=2,mactp,mact;
+
+int mtype,mxtype,mtm,mzz;
+int mzimen,mrzimen,mkasok,mmuki,mmukitm,mjumptm,mkeytm,mcleartm;
+int mmutekitm,mmutekion;
+int mztm,mztype;
+int actaon[7];
+//メッセージ message
+int mmsgtm,mmsgtype;
+
+int mascrollmax=21000;//9000
+
+
+
+
+//ブロック clock
+void tyobi(int x,int y,int type);
+void brockbreak(int t);
+#define tmax 641
+int tco;
+int ta[tmax],tb[tmax],tc[tmax],td[tmax],thp[tmax],ttype[tmax];
+int titem[tmax],txtype[tmax];
+
+//メッセージブロック message block
+int tmsgtm,tmsgtype,tmsgx,tmsgy,tmsgnobix,tmsgnobiy,tmsg;
+void ttmsg();void txmsg(string x,int a);
+void setfont(int x,int y);
+ 
+//効果を持たないグラ graphics that have no effect
+void eyobi(int xa,int xb,int xc,int xd,int xe,int xf,int xnobia,int xnobib,int xgtype,int xtm);
+#define emax 201
+int eco;
+int ea[emax],eb[emax],enobia[emax],enobib[emax],ec[emax],ed[emax];
+int ee[emax],ef[emax],etm[emax];
+int egtype[emax];
+
+
+
+//敵キャラ enemy character
+void ayobi(int xa,int xb,int xc,int xd,int xnotm,int xtype,int xxtype);
+void tekizimen();
+#define amax 24
+int aco;
+int aa[amax],ab[amax],anobia[amax],anobib[amax],ac[amax],ad[amax];
+int ae[amax],af[amax],abrocktm[amax];
+int aacta[amax],aactb[amax],azimentype[amax],axzimen[amax];
+int atype[amax],axtype[amax],amuki[amax],ahp[amax];
+int anotm[amax],anx[160],any[160];
+int atm[amax],a2tm[amax];
+int amsgtm[amax],amsgtype[amax];
+
+//敵出現 Enemy Appearance
+#define bmax 81
+int bco;
+int ba[bmax],bb[bmax],btm[bmax];
+int btype[bmax],bxtype[bmax],bz[bmax];
+
+
+//背景 background
+#define nmax 41
+int nxxmax,nco;
+int na[nmax],nb[nmax],nc[nmax],nd[nmax],ntype[nmax];
+int ne[nmax],nf[nmax],ng[nmax],nx[nmax];
+
+
+//リフト
+#define srmax 21
+int srco;
+int sra[srmax],srb[srmax],src[srmax],srd[srmax],sre[srmax],srf[srmax];
+int srtype[srmax],srgtype[srmax],sracttype[srmax],srsp[srmax];
+int srmuki[srmax],sron[srmax],sree[srmax];
+int srsok[srmax],srmovep[srmax],srmove[srmax];
+
+
+
+
+
+//スクロール範囲 scroll range
+int fx=0,fy=0,fzx,fzy,scrollx,scrolly;
+//全体のポイント whole point
+int fma=0,fmb=0;
+//強制スクロール forced scroll
+int kscroll=0;
+//画面サイズ(ファミコンサイズ×2)(256-224) => (Screen size (Famicom size x 2) (256-224))
+int fxmax=48000,fymax=42000;
+
+
+
+//ステージ stage 
+byte stagedate[17][2001];
+
+//画面黒 screen black
+int blacktm=1,blackx=0;screen black
+
+
+
+//自由な値 free value
+int xx[91];
+double xd[11];
+string xs[31];
+
+
+long stime;
---
+++ pure-game-framework/Source/Game/mygame_run.cpp
@@ -0,0 +1,103 @@
+#include "stdafx.h"
+#include "../Core/Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "../Library/audio.h"
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+#include "mygame.h"
+#include "colliders.h"
+#include <typeinfo>
+
+using namespace game_framework;
+/////////////////////////////////////////////////////////////////////////////
+// 這個class為遊戲的遊戲執行物件，主要的遊戲程式都在這裡
+/////////////////////////////////////////////////////////////////////////////
+CGameStateRun::CGameStateRun(CGame *g) : CGameState(g)
+{
+	int s = 0;
+}
+
+CGameStateRun::~CGameStateRun()
+{
+}
+
+void CGameStateRun::OnBeginState()
+{
+}
+
+void CGameStateRun::OnMove()							// 移動遊戲元素
+{
+	player.move();
+	Touching();
+}
+
+void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
+{
+	vector<string> player_image = { "resources/image/player/player_1.bmp" , "resources/image/player/player_2.bmp" ,"resources/image/player/player_1_flip.bmp" , "resources/image/player/player_2_flip.bmp", "resources/image/player/player_jump.bmp", "resources/image/player/player_jump_flip.bmp" };
+	player = Player(0, 0, player_image);
+	MapSetting(1);
+
+}
+
+void CGameStateRun::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags) {
+	if (nChar == VK_LEFT) {
+		
+		player.keyLeft = true;
+		player.keyRight = false;
+	}
+	if (nChar == VK_RIGHT) {
+		player.keyRight = true;
+		player.keyLeft = false;
+	}
+	if (nChar == VK_UP) {
+		player.keyUp = true;
+	}
+	if (nChar == VK_DOWN) {
+		player.keyDown = true;
+	}
+}
+
+void CGameStateRun::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	if (nChar == VK_RIGHT) {
+		player.keyRight = false;
+	}
+	if (nChar == VK_LEFT) {
+		player.keyLeft = false;
+	}
+	if (nChar == VK_UP) {
+		player.keyUp = false;
+	}
+	if (nChar == VK_DOWN) {
+		player.keyDown = false;
+	}
+}
+
+void CGameStateRun::OnLButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnLButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnMouseMove(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnRButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnRButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnShow()
+{
+	player.coll.ShowBitmap();
+	for (auto i : bricks_arr) {
+		i.coll.ShowBitmap();
+	}
+}
